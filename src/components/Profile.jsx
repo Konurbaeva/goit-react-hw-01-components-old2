887c2f981b97ab46f7f1c516ce090e3fff9d53ec
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-06-25 20:03:57 +0200

    added Profile component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,34 @@
+
+
+// stats={user.stats}
+export const Profile= ({ avatar, username, tag, location})=>{
+    return (
+        <div className="profile">
+        <div className="description">
+          <img
+            src={avatar}
+            alt={avatar}
+            className="avatar"
+          />
+          <p className="name">{username}</p>
+          <p className="tag">{tag}</p>
+          <p className="location">{location}</p>
+        </div>
+      
+        <ul className="stats">
+          <li>
+            <span className="label">Followers</span>
+            <span className="quantity">1000</span>
+          </li>
+          <li>
+            <span className="label">Views</span>
+            <span className="quantity">2000</span>
+          </li>
+          <li>
+            <span className="label">Likes</span>
+            <span className="quantity">3000</span>
+          </li>
+        </ul>
+      </div>
+    )
+}
