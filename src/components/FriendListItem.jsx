707dc5ddf94c5786c9b25737097c5b79fa594e12
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-06-27 00:42:57 +0200

    added color change regarding isOnline status

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -4,19 +4,23 @@ import styled from 'styled-components'
 //В зависимости от пропа isOnline, должен меняться цвет фона span.status.
 // Это можно сделать через разный CSS-класс или Styled Components.
 
-export const Span = styled.span`
-color: ${({theme}) => theme.colors.secondary}
-`;
+const Span = styled.span(({ isOnline }) => ({
+    background: isOnline ? 'green' : 'red'
+  }));
 
-export const FriendListItem = ({ friend}) => {
+export const FriendListItem = ({ friend }) => {
     const { name, isOnline, avatar, id } = friend;
-
+    
     return (
         <>
    <li className="item" key={id}>
-  <Span className={isOnline ? Span.color : 'offline'}></Span>
+  <Span 
+  isOnline={isOnline}
+   ></Span>
   <img className="avatar" src={avatar} alt="User avatar" width="48" />
-  <p className="name">{name}</p>
+  <Span 
+  isOnline={isOnline}
+   >{name}</Span>
 </li>
  </>
  )
