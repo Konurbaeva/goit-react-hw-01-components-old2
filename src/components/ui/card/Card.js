d5ba6bfdb1566f0d331fa4bfcdf3be3bbbc34ec1
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-06-28 16:39:35 +0200

    removed comments

--- src/components/ui/card/Card.js
+++ src/components/ui/card/Card.js
@@ -1,19 +1,5 @@
 import styled from 'styled-components';
 
-// export const Card = styled.div`
-// padding: 10px;
-// margin: 0 auto;
-// margin-top: 10px;
-// background-color:pink;
-// transition: 0.3s;
-// width: 20%;
-// align-items: center;
-// box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-// &:hover {
-//     box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-//     }
-// `
-
 export const Card = styled.div`
 padding: 10px;
 margin: 0 auto;
